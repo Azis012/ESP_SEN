bb7c3ab79d6d867871dbd2457942a50e1534e3af
Azis012 <111286224+Azis012@users.noreply.github.com>
2026-02-21 11:17:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
 
+let statusLampu = "OFF";
+
 app.get("/", (req, res) => {
-  res.send("Server ESP32 aktif 🚀");
+  res.send("Server Kontrol Lampu Aktif 🚗");
+});
+
+// Endpoint kontrol
+app.get("/kiri", (req, res) => {
+  statusLampu = "KIRI";
+  res.send("Lampu KIRI aktif");
+});
+
+app.get("/kanan", (req, res) => {
+  statusLampu = "KANAN";
+  res.send("Lampu KANAN aktif");
+});
+
+app.get("/hazard", (req, res) => {
+  statusLampu = "HAZARD";
+  res.send("Lampu HAZARD aktif");
+});
+
+app.get("/off", (req, res) => {
+  statusLampu = "OFF";
+  res.send("Lampu OFF");
+});
+
+// Endpoint untuk ESP32 baca status
+app.get("/status", (req, res) => {
+  res.send(statusLampu);
 });
 
 const PORT = process.env.PORT || 3000;
